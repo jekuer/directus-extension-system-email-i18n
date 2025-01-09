f2b37e07b610822aff7bdcb23a3b31d3fb95267a
Jens Kuerschner <code@jekuer.com>
2025-01-09 23:29:22 +0100

    in-code docu update

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export default defineHook(({ filter }, { services, getSchema, env }) => {
 			// take IP from the server this script is running on
 			const systemIp = ip.address();
 			// preparing the accountability object to be able searching directus users with admin rights
+			// this includes a lot of empty props, which is necessary to match the type of the accountability object
 			const adminAccountability:Accountability = { role: null, roles: [], user: null, admin: true, app: false, ip: systemIp, origin: 'user language lookup by extension' };
 			// get the language from searching and reading the user
 			const users = new ItemsService('directus_users', { schema: await getSchema(), accountability: adminAccountability }); 
